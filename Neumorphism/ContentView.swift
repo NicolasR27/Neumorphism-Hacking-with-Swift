95c448b35fc10e93990c1da654cf337add5f43c2
Nicolas Rios <nickrios33@yahoo.com>
2020-02-27 00:27:31 -0800

    added zStack and fill and shadows

--- Neumorphism/ContentView.swift
+++ Neumorphism/ContentView.swift
@@ -61,6 +61,26 @@ struct SimpleButtonStyle: ButtonStyle{
     
 }
 
+struct DarkBackground<S: Shape>: View {
+    var isHighlighted:Bool
+    var shape: S
+    
+    var body: some View{
+        ZStack{
+            if isHighlighted{
+                shape
+                    .fill(Color.darkEnd)
+                    .shadow(color: Color.darkStart, radius:10, x: 5, y: 5)
+                  .shadow(color: Color.darkEnd, radius:10, x: -5, y: -5)
+                
+            }
+        }
+        
+    }
+    
+}
+
+
 struct ContentView: View {
     var body: some View {
         ZStack{
