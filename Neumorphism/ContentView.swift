0a9582982a480e2352aec849e8f6fc8778cb0017
Nicolas Rios <nickrios33@yahoo.com>
2020-02-27 00:04:06 -0800

    added mask around hear system item

--- Neumorphism/ContentView.swift
+++ Neumorphism/ContentView.swift
@@ -13,25 +13,40 @@ extension Color {
     static let offWhite = Color(red:225/255,green:225/255,blue:233/255)
 }
 
+extension LinearGradient{
+    init(_ colors: Color...) {
+    self.init(gradient: Gradient(colors: colors), startPoint: .topLeading, endPoint: .bottomTrailing)
+    }
+}
 struct SimpleButtonStyle: ButtonStyle{
     func makeBody(configuration:Self.Configuration) -> some View{
         configuration.label
             .padding(30)
+            .contentShape(Circle())
             .background(
                 Group{
                     if configuration.isPressed{
                         Circle()
                             .fill(Color.offWhite)
-                    }else{
+                        .overlay(
+                        Circle()
+                            .stroke(Color.gray, lineWidth: 4)
+                            .blur(radius: 4)
+                            .offset(x:2,y:2)
+                            .mask(Circle().fill(LinearGradient(Color.black,Color.clear)))
+                            
+                        )
+                            
+                    } else {
                         Circle()
                             .fill(Color.offWhite)
                             .shadow(color:Color.black.opacity(0.2),radius: 10,x:10,y:10)
                             .shadow(color:Color.white.opacity(0.7),radius: 10,x:-5,y:-5)
-            
+                        
+                    }
+                    
                 }
-            
-        }
-    )}
+        )}
     
 }
 
