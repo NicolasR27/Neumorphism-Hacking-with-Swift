5bf8d0b0dad3ebc667dff568403ee74b0281b7e8
Nicolas Rios <nickrios33@yahoo.com>
2020-02-26 23:37:55 -0800

    adde system heart and added button

--- Neumorphism/ContentView.swift
+++ Neumorphism/ContentView.swift
@@ -8,9 +8,48 @@
 
 import SwiftUI
 
+
+extension Color {
+    static let offWhite = Color(red:225/255,green:225/255,blue:233/255)
+}
+
+struct SimpleButtonStyle: ButtonStyle{
+    func makeBody(configuration:Self.Configuration) -> some View{
+        configuration.label
+            .padding(30)
+            .background(
+                Group{
+                    if configuration.isPressed{
+                        Circle()
+                            .fill(Color.offWhite)
+                    }else{
+                        Circle()
+                            .fill(Color.offWhite)
+                            .shadow(color:Color.black.opacity(0.2),radius: 10,x:10,y:10)
+                            .shadow(color:Color.white.opacity(0.7),radius: 10,x:-5,y:-5)
+            
+                }
+            
+        }
+    )}
+    
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack{
+            Color.offWhite
+            
+            Button(action: {
+                print("button tapped")
+            }){
+                Image(systemName:"heart.fill")
+                    .foregroundColor(.gray)
+            }
+            .buttonStyle(SimpleButtonStyle())
+            
+        }
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
