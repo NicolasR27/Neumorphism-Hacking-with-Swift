4b5a8f9e26c6c94fa47769ca6c703fe58c91365c
Nicolas Rios <nickrios33@yahoo.com>
2020-02-27 23:53:18 -0800

    added darkButton style

--- Neumorphism/ContentView.swift
+++ Neumorphism/ContentView.swift
@@ -14,6 +14,10 @@ extension Color {
     
     static let darkStart = Color(red: 50/255, green: 60/255, blue: 65/255)
     static let darkEnd = Color(red: 25/255, green: 25/555, blue:30/255 )
+    
+    static let lightStart = Color(red: 60 / 255, green: 160 / 255, blue: 240 / 255)
+       static let lightEnd = Color(red: 30 / 255, green: 80 / 255, blue: 120 / 255)
+    
 }
 
 extension LinearGradient{
@@ -69,13 +73,16 @@ struct DarkBackground<S: Shape>: View {
         ZStack {
             if isHighlighted {
                 shape
-                    .fill(Color.darkEnd)
+                    .fill(LinearGradient(Color.darkEnd,Color.darkStart))
+                    .overlay(shape.stroke(LinearGradient(Color.darkStart,Color.darkEnd),lineWidth: 4))
                     .shadow(color: Color.darkStart, radius: 10, x: 5, y: 5)
                     .shadow(color: Color.darkEnd, radius: 10, x: -5, y: -5)
                 
             } else {
                 shape
-                    .fill(Color.darkEnd)
+                    .fill(LinearGradient(Color.darkStart,Color.darkEnd))
+                    .overlay(shape.stroke(Color.darkEnd,lineWidth: 4))
+                    
                     .shadow(color: Color.darkStart, radius: 10, x: -10, y: -10)
                     .shadow(color: Color.darkEnd, radius: 10, x: 10, y: 10)
             }
@@ -83,6 +90,28 @@ struct DarkBackground<S: Shape>: View {
     }
 }
 
+struct ColorfulBackground<S: Shape>: View {
+    var isHighlighted: Bool
+    var shape: S
+
+    var body: some View {
+        ZStack {
+            if isHighlighted {
+                shape
+                    .fill(LinearGradient(Color.lightEnd, Color.lightStart))
+                    .overlay(shape.stroke(LinearGradient(Color.lightStart, Color.lightEnd), lineWidth: 4))
+                    .shadow(color: Color.darkStart, radius: 10, x: 5, y: 5)
+                    .shadow(color: Color.darkEnd, radius: 10, x: -5, y: -5)
+            } else {
+                shape
+                    .fill(LinearGradient(Color.darkStart, Color.darkEnd))
+                    .overlay(shape.stroke(LinearGradient(Color.lightStart, Color.lightEnd), lineWidth: 4))
+                    .shadow(color: Color.darkStart, radius: 10, x: -10, y: -10)
+                    .shadow(color: Color.darkEnd, radius: 10, x: 10, y: 10)
+            }
+        }
+    }
+}
 
 
 struct DarkButtonStyle: ButtonStyle{
@@ -94,25 +123,87 @@ struct DarkButtonStyle: ButtonStyle{
                 DarkBackground(isHighlighted: configuration.isPressed, shape: Circle())
                 
         )
+            .animation(nil)
+        
     }
 }
+
+struct DarkToggleStyle: ToggleStyle {
+    func makeBody(configuration: Self.Configuration) -> some View{
+        Button(action:{
+            configuration.isOn.toggle()
+            
+        }){
+            configuration.label
+                .padding(30)
+                .contentShape(Circle())
+        }
+        .background(
+            DarkBackground(isHighlighted:configuration.isOn,shape: Circle())
+        )
+    }
+    
+}
+struct ColorfulButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .padding(30)
+            .contentShape(Circle())
+            .background(
+                ColorfulBackground(isHighlighted: configuration.isPressed, shape: Circle())
+        )
+            .animation(nil)
+    }
+}
+
+struct ColorfulToggleStyle: ToggleStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        Button(action: {
+            configuration.isOn.toggle()
+        }) {
+            configuration.label
+                .padding(30)
+                .contentShape(Circle())
+        }
+        .background(
+            ColorfulBackground(isHighlighted: configuration.isOn, shape: Circle())
+        )
+    }
+}
+
+
+
 struct ContentView: View {
+    @State private var IsToggled = false
+    
+    
     var body: some View {
+        
         ZStack{
             LinearGradient(Color.darkStart,Color.darkEnd)
             
             
-            Button(action: {
-                print("button tapped")
-            }){
-                Image(systemName:"heart.fill")
-                    .foregroundColor(.gray)
+            VStack(spacing: 40) {
+                Button(action: {
+                    print("button tapped")
+                }){
+                    Image(systemName:"heart.fill")
+                        .foregroundColor(.white)
+                }
+                .buttonStyle(DarkButtonStyle())
+                
+                Toggle(isOn: $IsToggled) {
+                    Image(systemName: "heart.fill")
+                        .foregroundColor(.white)
+                }
+                .toggleStyle(ColorfulToggleStyle())
+                
             }
-            .buttonStyle(DarkButtonStyle())
             
         }
         .edgesIgnoringSafeArea(.all)
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
